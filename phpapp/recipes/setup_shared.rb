b42b83c5662bad3e906e91463e7d854882ebcab3
Forrest Middleton <forrest@dealr.cloud>
2019-07-31 17:44:56 -0600

    New composer install

--- phpapp/recipes/setup_shared.rb
+++ phpapp/recipes/setup_shared.rb
@@ -34,11 +34,24 @@ bash 'install_composer' do
   interpreter "bash"
   user 'root'
   code <<-EOH
+        #!/bin/sh
+
+        EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
         php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-        php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-        php composer-setup.php
-        php -r "unlink('composer-setup.php');"
+        ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+        if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+        then
+            >&2 echo 'ERROR: Invalid installer signature'
+            rm composer-setup.php
+            exit 1
+        fi
+
+        php composer-setup.php --quiet
+        RESULT=$?
         mv composer.phar /usr/local/bin/composer
+        rm composer-setup.php
+        exit $RESULT
     EOH
 end
 
@@ -66,7 +79,7 @@ bash 'install_extensions' do
 
     if [ -z "$v8installed" ]
     then
-        printf "/opt/v8\n" | pecl install v8js
+        printf "/opt/v8\n" | pecl install v8js-2.1.0
     fi
 
     imagickinstalled="$(pecl list | grep -i imagick)"
